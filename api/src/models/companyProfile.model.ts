498a0c0e93997593d909884633c75bb4f8af19a8
24LP8740 24LP8740 <24LP8740@G401-CC0119.local>
2025-06-09 17:58:32 +0800

    create companyProfile model

---
+++ api/src/models/companyProfile.model.ts
@@ -0,0 +1,70 @@
+import { Schema, model } from "mongoose";
+const companyProfile = new Schema({
+email:{
+    type:String,
+    required:true
+
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phoneNumber:{
+    type:String,
+     required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  background: {
+    type: String,
+    required: true,
+  },
+  AvatarImage: {
+    type: String,
+    required: true,
+  },
+
+ since:{
+    type:String,
+     required: true,
+ },
+
+  websiteURL:{
+     type:String,
+     required: true,
+
+  },
+  about:{
+    type:String,
+     required: true,
+  },
+  packages:{
+    type: Schema.Types.ObjectId,
+    ref: "package",
+    required: true,
+  },
+ availableDestinations:{
+     type: Schema.Types.ObjectId,
+    ref: "destination",
+    required: true,
+ },
+ reviews:{
+     type: Number,
+    required: true,
+ },
+ Rating:{
+    type: Number,
+    required: true,
+ },
+    createdAt: {
+    type: Date,
+    required: true,
+  },
+  updatedAt: {
+    type: Date,
+    required: true,
+  },
+});
+export const foodModel = model("companyProfile", companyProfile);
