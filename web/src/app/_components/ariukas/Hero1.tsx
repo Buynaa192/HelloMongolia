06e8018c0834a57af28f44af9ad068cc5f5b2907
Xthgooo <ariunzulbaigalmaa@gmail.com>
2025-06-11 23:55:15 +0800

    homepagedfixed

--- web/src/app/_components/ariukas/Hero1.tsx
+++ web/src/app/_components/ariukas/Hero1.tsx
@@ -1,26 +1,29 @@
-import Image from "next/image";
-
-import Hero1Text from "./Hero1Text";
+import { Hero1Text } from "./Hero1Text";
 
 export const Hero1 = () => {
-  return (
-    <div className="relative h-screen w-full">
-      <div className="relative w-full h-screen flex flex-col">
-        <Image
-          src="/images/ariukasImages/sky.jpg"
-          alt="Mongolian ger"
-          fill
-          className="object-cover"
-          priority
-        />
-        <Hero1Text />
-      </div>
-      <div
-        className="absolute bottom-0 left-0 w-full h-130 pointer-events-none"
-        style={{
-          background: "linear-gradient(to bottom, transparent, white)",
-        }}
-      />
-    </div>
-  );
+	return (
+		<div className="relative h-screen w-full">
+			<div className="relative w-full h-screen flex flex-col">
+				<video
+					className="absolute inset-0 w-full h-full object-cover z-0"
+					autoPlay
+					muted
+					loop
+					playsInline>
+					<source
+						src="/images/ariukasImages/855785-hd_1920_1080_24fps.mp4"
+						type="video/mp4"
+					/>
+				</video>
+				<Hero1Text />
+			</div>
+
+			<div
+				className="absolute bottom-0 left-0 w-full h-130 pointer-events-none"
+				style={{
+					background: "linear-gradient(to bottom, transparent, white)",
+				}}
+			/>
+		</div>
+	);
 };
--- web/src/app/_components/ariukas/Hero2Text.tsx
+++ web/src/app/_components/ariukas/Hero2Text.tsx
@@ -1,32 +1,42 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { motion } from "framer-motion";
 
 export const Hero2Text = () => {
-  return (
-    <div className="absolute top-0 w-full  text-black py-8 px-8 md:px-16 flex flex-col z-30 items-center gap-12">
-      <div className="flex w-full justify-between md:flex-row md:flex flex-col gap-8 items-center">
-        <div className="w-fit lg:text-left md:text-left text-center">
-          <h1 className="lg:text-6xl text-5xl font-extrabold leading-tight ">
-            WHY
-            <br />
-            <span className="block mt-2">
-              GO <br /> MON<span className="text-blue-600">GO</span>LIA ?
-            </span>
-          </h1>
-        </div>
-        <div className="md:w-1/2 text-[16px] md:text-[18px] lg:text-xl  text-center md:text-left flex flex-col gap-8">
-          <p className="w-full text-start">
-            Because it's one of the last places on Earth where you can truly
-            disconnect—and reconnect. Vast untouched landscapes, ancient
-            traditions, and warm-hearted nomadic hospitality offer not just a
-            trip, but a transformation.
-          </p>
-          <p className="w-full">
-            If you're seeking something real, raw, and unforgettable, <br />
-            <span className="font-bold text-2xl">Mongolia is calling.</span>
-          </p>
-        </div>
-      </div>
-      <Button className="w-fit">EXPLORE ALL EXPERIENCES</Button>
-    </div>
-  );
+	return (
+		<motion.div
+			initial={{ y: 90, opacity: 0 }}
+			whileInView={{ y: 0, opacity: 1 }}
+			transition={{ duration: 1, ease: "easeOut" }}
+			viewport={{ once: true }}
+			className="absolute inset-0 z-20 flex items-center justify-center text-white">
+			<div className="absolute top-0 w-full  text-black py-8 px-8 md:px-16 flex flex-col z-10 items-center gap-12 z">
+				<div className="flex w-full justify-between md:flex-row md:flex flex-col gap-8 items-center">
+					<div className="md:text-left text-center">
+						<h1 className="lg:text-6xl text-5xl font-extrabold leading-tight ">
+							WHY
+							<br />
+							<span className="block mt-2">
+								GO <br /> MON<span className="text-blue-600">GO</span>LIA ?
+							</span>
+						</h1>
+					</div>
+					<div className="md:w-1/2 text-[16px] md:text-[18px] lg:text-xl flex flex-col gap-8">
+						<p className="w-full md:text-left text-center ">
+							Because it's one of the last places on Earth where you can truly
+							disconnect—and reconnect. Vast untouched landscapes, ancient
+							traditions, and warm-hearted nomadic hospitality offer not just a
+							trip, but a transformation.
+						</p>
+						<p className="w-full md:text-left text-center text-white">
+							If you're seeking something real, raw, and unforgettable, <br />
+							<span className="font-bold text-2xl">Mongolia is calling.</span>
+						</p>
+					</div>
+				</div>
+				<Button className="w-fit">EXPLORE ALL EXPERIENCES</Button>
+			</div>
+		</motion.div>
+	);
 };
