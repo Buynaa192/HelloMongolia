525dc450678b050c14ce4c28c2c60e2d13b1ea88
24LP9131 24LP9131 <24LP9131@G000-CC0000.local>
2025-06-12 15:11:14 +0800

    pakcages

--- web/src/app/travel-plans/page.tsx
+++ web/src/app/travel-plans/page.tsx
@@ -2,12 +2,12 @@ import { FilteredPackages } from "./_components/filteredPackages";
 import { Search } from "./_components/search";
 import { SearchFilter } from "./_components/searchFIlter";
 import { TravelPlanHome } from "./_components/travelPLanHome";
-import SmoothScroll from "./assets/smoothScroll";
+// import SmoothScroll from "./assets/smoothScroll";
 
 export default function PackagesExplore() {
   return (
     <div className="w-full h-full ">
-      <SmoothScroll />
+      {/* <SmoothScroll /> */}
       <TravelPlanHome />
       <div className="bg-white w-full p-2 ">HOME | EXPLORE PLANS</div>
       <Search />
