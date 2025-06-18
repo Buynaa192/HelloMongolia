6ec9d0a4fda843190ed6451240fe338005c0b0ca
24LP5333 24LP5333 <24LP5333@G401-CC0116.local>
2025-06-18 20:15:09 +0800

    sanahgu bno

--- web/src/components/buynaasComponents/destinationPackage/desPackage.tsx
+++ web/src/components/buynaasComponents/destinationPackage/desPackage.tsx
@@ -3,6 +3,7 @@
 import { PackageType } from "@/app/_providers/AuthProvider";
 import { api } from "@/axios";
 import Image from "next/image";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 type idType = {
   id: string;
@@ -17,21 +18,24 @@ export const DesdinationPackage = ({ id }: idType) => {
     };
     GetPackage();
   }, []);
+  console.log(packages);
 
   return (
     <div className="w-full grid grid-cols-5 border-2 h-100 gap-4 p-4">
       {packages.map((item, indx) => {
         return (
-          <div className="border-2" key={indx}>
-            <Image
-              className="w-full h-[calc(100%-40px)]"
-              alt={item.coverPhoto}
-              src={item.coverPhoto}
-              width={500}
-              height={100}
-            ></Image>
-            <p>{item.title}</p>
-          </div>
+          <Link key={indx} href={"/"}>
+            <div className="border-2">
+              <Image
+                className="w-full h-[calc(100%-40px)]"
+                alt={item.coverPhoto}
+                src={item.coverPhoto}
+                width={500}
+                height={100}
+              ></Image>
+              <p>{item.title}</p>
+            </div>
+          </Link>
         );
       })}
     </div>
