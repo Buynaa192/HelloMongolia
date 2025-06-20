32d70f2480248b5a0434efac2f04e33eb397ea74
24LP1577 24LP1577 <24LP1577@G401-CC0100.local>
2025-06-20 23:13:07 +0800

    homepage searchinput oruldag bolov

--- web/src/app/_components/ariukasComponents/Hero1Text.tsx
+++ web/src/app/_components/ariukasComponents/Hero1Text.tsx
@@ -2,10 +2,13 @@
 
 import { useEffect, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
+import { SearchInMongolia } from "./SearchMongolia";
 const animatedWords = ["DISCOVER", "EXPLORE", "EXPERIENCE", "FEEL"];
 
 export const Hero1Text = () => {
   const [index, setIndex] = useState(0);
+  const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -16,38 +19,35 @@ export const Hero1Text = () => {
 
   return (
     <div className="absolute flex flex-col gap-8 items-center justify-center w-full h-full">
-      <div className="flex flex-col items-center font-extrabold text-5xl lg:text-7xl">
-        <div className="flex justify-center ">
-          <AnimatePresence mode="wait">
-            <motion.div
-              key={animatedWords[index]}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 0.8 }}
-            >
-              {animatedWords[index]}
-            </motion.div>
-          </AnimatePresence>
+      {!query || loading ? (
+        <div className="flex flex-col items-center font-extrabold text-5xl lg:text-7xl">
+          <div className="flex justify-center ">
+            <AnimatePresence mode="wait">
+              <motion.div
+                key={animatedWords[index]}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.8 }}
+              >
+                {animatedWords[index]}
+              </motion.div>
+            </AnimatePresence>
+          </div>
+          <div className="text-white">
+            <span>MONGOLIA</span>
+          </div>
         </div>
-        <div className="text-white">
-          <span>MONGOLIA</span>
-        </div>
-      </div>
+      ) : (
+        <div></div>
+      )}
 
-      <form className="flex items-center border border-white rounded-md overflow-hidden">
-        <input
-          type="text"
-          placeholder=" 🇲🇳 Search in Mongolia  "
-          className="px-4 py-3 text-lg text-white bg-transparent placeholder-white focus:outline-none w-full"
-        />
-        <button
-          type="submit"
-          className="px-4 py-3 text-lg text-black hover:bg-gray-200 transition"
-        >
-          🔍
-        </button>
-      </form>
+      <SearchInMongolia
+        query={query}
+        setQuery={setQuery}
+        loading={loading}
+        setLoading={setLoading}
+      />
     </div>
   );
 };
