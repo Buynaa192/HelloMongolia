c3a47ffb340c058b71e0018c266c1779ddede1a8
Tserendagva Buyanzayaa <tserendagvabuyanzayaa@Buynaa-MacBook-Pro.local>
2025-06-20 16:11:20 +0800

    buildin aldaa zassan

--- web/src/app/explore-destinations/page.tsx
+++ web/src/app/explore-destinations/page.tsx
@@ -9,7 +9,7 @@ import { Regions } from "@/components/buynaasComponents/regionsComponents/region
 import { PopularDestination } from "@/components/buynaasComponents/popularDestinations.tsx/popularDestination";
 import { api } from "@/axios";
 import { DestinationType } from "../_providers/AuthProvider";
-import { BackToHomePathButtons } from "../_components/ariukasComponents/BackToHomePagePathButtons";
+
 
 const images = [
   "https://res.cloudinary.com/df60cobe2/image/upload/v1750344900/zvtv2v8ujkwank2sed1x.jpg",
