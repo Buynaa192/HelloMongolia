d07d623ca016398e2ff918f047d459c2bcf7d5f6
24LP5333 24LP5333 <24LP5333@G401-CC0116.local>
2025-06-23 21:06:48 +0800

    region uurcilsun

--- web/src/components/buynaasComponents/regionsComponents/regionCard.tsx
+++ web/src/components/buynaasComponents/regionsComponents/regionCard.tsx
@@ -8,13 +8,12 @@ type itemType = {
 };
 
 export const RegionCard = ({ item }: itemType) => {
-  const [videoSource, setVideoSource] = useState(item.videoUrl);
   return (
     <div className="w-full h-80 relative group rounded-lg overflow-hidden border border-gray-300 shadow-sm hover:shadow-md transition-shadow duration-300">
       <div className="absolute inset-0 bg-black/40 opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-10" />
 
       <div className="w-full h-[calc(100%-40px)] relative">
-        {videoSource &&
+        {/* {videoSource &&
           (videoSource.endsWith(".mp4") ? (
             <video
               key={videoSource}
@@ -33,7 +32,7 @@ export const RegionCard = ({ item }: itemType) => {
               fill
               className="object-cover"
             />
-          ))}
+          ))} */}
 
         <span className="absolute inset-0 flex items-center justify-center text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-20 text-lg font-semibold">
           See more
