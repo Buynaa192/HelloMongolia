a69a42b4966426f8bc57bd8e69e467f44b094f36
24LP5333 24LP5333 <24LP5333@G401-CC0116.local>
2025-06-24 15:04:09 +0800

    region page

--- web/src/app/explore-destinations/[region]/page.tsx
+++ web/src/app/explore-destinations/[region]/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 import { BackToHomePathButtons } from "@/app/_components/ariukasComponents/BackToHomePagePathButtons";
-// import { DestinationType } from "@/app/_providers/AuthProvider";
+import { DestinationType, RegionType } from "@/app/_providers/AuthProvider";
 import { api } from "@/axios";
-// import { DestinationCard } from "@/components/buynaasComponents/popularDestinations.tsx/destinationCard";
+import { DestinationCard } from "@/components/buynaasComponents/popularDestinations.tsx/destinationCard";
 
 import { useParams } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 type paramsType = {
   region: string;
@@ -13,27 +13,34 @@ type paramsType = {
 
 export default function RegionPage() {
   const region = useParams<paramsType>();
+  const [regn, setRegn] = useState<RegionType>();
+  const getRegions = async () => {
+    const res = await api.get(`/regions?regionName=${region.region}`);
+    setRegn(res.data.region);
+  };
+  console.log(regn);
 
+  const getDestination = async () => {
+    const res = await api.get(`/regions/destinations/68592416611c9aae4411aaa2`);
+    console.log(res.data);
+  };
   useEffect(() => {
-    const regionsasdas = async () => {
-      const res = await api.get(`/regions?regionID=${region.region}`);
-      console.log(res.data);
-    };
-    regionsasdas();
+    getRegions();
+    getDestination();
   }, []);
 
   return (
     <div className="w-full h-full text-white flex flex-col gap-4">
       <div className="relative w-full h-[800px] overflow-hidden">
-        {/* <video
-          key={videoSource}
+        <video
+          key={regn?.videoUrl}
           className="w-full h-full object-cover"
           autoPlay
           muted
           loop
           playsInline
-          src={videoSource}
-        /> */}
+          src={regn?.videoUrl}
+        />
 
         <div className="absolute inset-0 flex flex-col items-center justify-center gap-4 text-white bg-black/30 px-4 text-center">
           <p className="font-bold text-[60px]">{region.region}</p>
@@ -41,34 +48,28 @@ export default function RegionPage() {
       </div>
 
       <BackToHomePathButtons />
-      {/* <p className="m-4">{currentImages.description}</p> */}
+      <p className="m-4">{regn?.description}</p>
       <div className="w-full flex justify-center mb-20">
-        <div className="w-full max-w-4xl aspect-video">
+        {/* <div className="w-full max-w-4xl aspect-video">
           <iframe
             className="w-full h-full rounded-lg"
-            // src={currentImages.video}
+            src={regn?.videoUrl}
             title="Sample Video"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
           ></iframe>
-        </div>
+        </div> */}
       </div>
       <div className="flex flex-col gap-3 m-4">
         <p className="font-bold text-2xl ml-10">Destinations</p>
         <div className="w-full grid grid-cols-4 gap-4">
-          {/* {regionDestinations.map((item, i) => (
-            <DestinationCard key={i} item={item} />
-          ))} */}
+          {/* <DestinationCard /> */}
         </div>
       </div>
       <div className="flex flex-col gap-3 m-4">
         <p className="font-bold text-2xl ml-10">Packages:</p>
-        <div className="w-full grid grid-cols-4 gap-4">
-          {/* {regionDestinations.map((item, i) => (
-            <DestinationCard key={i} item={item} />
-          ))} */}
-        </div>
+        <div className="w-full grid grid-cols-4 gap-4"></div>
       </div>
     </div>
   );
