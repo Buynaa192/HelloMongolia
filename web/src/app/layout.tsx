3c6401dbf6ad05d93074d75a332c75cc3f0bf404
24LP9131 24LP9131 <24LP9131@G000-CC0000.local>
2025-06-24 15:18:16 +0800

    package

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,8 +1,12 @@
 import { Toaster } from "sonner";
 import { AuthProvider } from "./_providers/AuthProvider";
 import "./globals.css";
+<<<<<<< HEAD
 import { Footer } from "./_components/ariukasComponents/Footer";
 
+=======
+import { Suspense } from "react";
+>>>>>>> 4c61afa (package)
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -10,12 +14,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+<<<<<<< HEAD
       <body className="w-screen flex justify-center bg-black h-full flex-col items-center">
         <AuthProvider>
           <div className="max-w-[1440px] w-full h-full mb-10">{children}</div>
           <Footer />
         </AuthProvider>
         <Toaster />
+=======
+      <body className="w-screen flex justify-center bg-black h-full">
+        <Suspense fallback={<div>Loading travel plans...</div>}>
+          <AuthProvider>
+            <div className="max-w-[1440px] w-full h-full">{children}</div>
+          </AuthProvider>
+          <Toaster />
+        </Suspense>
+>>>>>>> 4c61afa (package)
       </body>
     </html>
   );
