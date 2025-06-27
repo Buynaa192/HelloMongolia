54d487f33610403a0538fbc8d4aa138b58e33ed5
24LP6734 24LP6734 <24LP6734@G401-CC0106.local>
2025-06-27 16:55:40 +0800

    design changes

--- web/src/app/(customer)/travel-plans/_components/searchFIlter.tsx
+++ web/src/app/(customer)/travel-plans/_components/searchFIlter.tsx
@@ -32,30 +32,51 @@ export const SearchFilter: React.FC<SearchFilterProps> = ({
   selectedActivities,
   setSelectedActivities,
 }) => {
-  const tripTypes: TripType[] = ["Scenery", "Adventure", "Cultural", "Historical", "Family", "Scientific", "Festival & Events", "Off-road"];
+  const tripTypes: TripType[] = [
+    "Scenery",
+    "Adventure",
+    "Cultural",
+    "Historical",
+    "Family",
+    "Scientific",
+    "Festival & Events",
+    "Off-road",
+  ];
   const cost = ["0-500$", "500-1000$", "1000-5000$", "5000$+"];
   const duration = ["1 day", "1-7 days", "7-14 days", "14-21 days", "21+ days"];
 
   const handleTripTypeChange = (type: TripType) => {
-    setSelectedTripTypes((prev) => (prev.includes(type) ? prev.filter((t) => t !== type) : [...prev, type]));
+    setSelectedTripTypes((prev) =>
+      prev.includes(type) ? prev.filter((t) => t !== type) : [...prev, type]
+    );
   };
 
   const handleCostChange = (range: string) => {
-    setSelectedCosts((prev) => (prev.includes(range) ? prev.filter((c) => c !== range) : [...prev, range]));
+    setSelectedCosts((prev) =>
+      prev.includes(range) ? prev.filter((c) => c !== range) : [...prev, range]
+    );
   };
 
   const handleDurationChange = (range: string) => {
-    setSelectedDurations((prev) => (prev.includes(range) ? prev.filter((d) => d !== range) : [...prev, range]));
+    setSelectedDurations((prev) =>
+      prev.includes(range) ? prev.filter((d) => d !== range) : [...prev, range]
+    );
   };
 
   const handleActivityChange = (activity: ActivityType) => {
-    setSelectedActivities((prev) => (prev.some((a) => a._id === activity._id) ? prev.filter((a) => a._id !== activity._id) : [...prev, activity]));
+    setSelectedActivities((prev) =>
+      prev.some((a) => a._id === activity._id)
+        ? prev.filter((a) => a._id !== activity._id)
+        : [...prev, activity]
+    );
   };
 
   return (
     <div className="w-130 h-fit bg-transparent flex-col flex items-center relative z-10 pl-20 ">
       <div className="w-full h-[40px] flex items-center justify-between  ">
-        <div className=" font-semibold  w-70  text-white text-3xl ">All Search Filters</div>
+        <div className=" font-semibold  w-70  text-white text-3xl ">
+          All Search Filters
+        </div>
         <div className="flex h-10  items-center justify-end p-4">
           <Button
             onClick={clearAllFilters}
@@ -77,7 +98,7 @@ export const SearchFilter: React.FC<SearchFilterProps> = ({
                   <Badge
                     key={type}
                     onClick={() => handleTripTypeChange(type)}
-                    className={`pt-2 px-4 border-none cursor-pointer transition-colors duration-200 text-sm font-medium group flex flex-col items-center  ${
+                    className={`pt-2 px-4 border-none cursor-pointer transition-colors duration-200 text-sm font-medium group flex flex-col items-center max-2xl:text-[12px] ${
                       selected ? "bg-white/40 text-white" : "border-accent"
                     }`}
                   >
@@ -103,7 +124,7 @@ export const SearchFilter: React.FC<SearchFilterProps> = ({
                   <Badge
                     key={range}
                     onClick={() => handleCostChange(range)}
-                    className={`pt-2 px-4 border-none cursor-pointer transition-colors duration-200 text-sm font-medium group flex flex-col items-center  ${
+                    className={`pt-2 px-4 border-none cursor-pointer transition-colors duration-200 text-sm font-medium group flex flex-col items-center max-2xl:text-[12px] ${
                       selected ? "bg-white/40 text-white" : "border-accent"
                     }`}
                   >
@@ -129,7 +150,7 @@ export const SearchFilter: React.FC<SearchFilterProps> = ({
                   <Badge
                     key={range}
                     onClick={() => handleDurationChange(range)}
-                    className={`pt-2 px-4 border-0 cursor-pointer transition-colors duration-200 text-sm font-medium group flex flex-col items-center  ${
+                    className={`pt-2 px-4 border-0 cursor-pointer transition-colors duration-200 text-sm font-medium group flex flex-col items-center max-2xl:text-[12px] ${
                       selected ? "bg-white/40 text-white" : "border-accent"
                     }`}
                   >
@@ -150,12 +171,14 @@ export const SearchFilter: React.FC<SearchFilterProps> = ({
             <div className="text-2xl font-semibold  ">Activities</div>
             <div className="flex flex-wrap gap-3">
               {allActivities.map((activity) => {
-                const selected = selectedActivities.some((a) => a._id === activity._id);
+                const selected = selectedActivities.some(
+                  (a) => a._id === activity._id
+                );
                 return (
                   <Badge
                     key={activity._id}
                     onClick={() => handleActivityChange(activity)}
-                    className={`pt-2 px-4 border-0 cursor-pointer transition-colors duration-200 text-sm font-medium group flex flex-col items-center  ${
+                    className={`pt-2 px-4 border-0 cursor-pointer transition-colors duration-200 text-sm font-medium group flex flex-col items-center max-2xl:text-[12px] ${
                       selected ? "bg-white/40 text-white" : "border-accent"
                     }`}
                   >
