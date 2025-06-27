3aa1e98a02211c30f7382af133d5a672f3bfdcef
Tserendagva Buyanzayaa <tserendagvabuyanzayaa@Buynaa-MacBook-Pro.local>
2025-06-27 16:26:58 +0800

    back destination deer region populate hiideg bolson

--- web/src/components/buynaasComponents/popularDestinations.tsx/popularDestination.tsx
+++ web/src/components/buynaasComponents/popularDestinations.tsx/popularDestination.tsx
@@ -7,16 +7,13 @@ type destinationType = {
   destination: DestinationType[];
   regions: RegionType[];
 };
-export const PopularDestination = ({
-  destination,
-  regions,
-}: destinationType) => {
+export const PopularDestination = ({ destination }: destinationType) => {
   return (
     <div className="flex flex-col gap-3 p-5">
       <p className="font-bold text-3xl ml-5">Popular destinations</p>
       <div className="w-full grid grid-cols-5 gap-4">
         {destination.map((item, i) => (
-          <PopularDestinationCard key={i} item={item} regions={regions} />
+          <PopularDestinationCard key={i} item={item} />
         ))}
       </div>
     </div>
