f6aeb727e42d630dd89c72eea872471fe42a94bd
buynaa <tserendagvabuyanzayaa@Buynaa-MacBook-Pro.local>
2025-06-27 16:56:51 +0800

    destinationi zurag zassan

--- web/src/components/buynaasComponents/popularDestinations.tsx/popularDestination.tsx
+++ web/src/components/buynaasComponents/popularDestinations.tsx/popularDestination.tsx
@@ -5,12 +5,11 @@ import { PopularDestinationCard } from "./popularDestinationCard";
 
 type destinationType = {
   destination: DestinationType[];
-  regions: RegionType[];
 };
 export const PopularDestination = ({ destination }: destinationType) => {
   return (
     <div className="flex flex-col gap-3 p-5">
-      <p className="font-bold text-3xl ml-5">Popular destinations</p>
+      <p className="font-bold text-3xl ml-5">All destinations</p>
       <div className="w-full grid grid-cols-5 gap-4">
         {destination.map((item, i) => (
           <PopularDestinationCard key={i} item={item} />
